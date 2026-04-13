83550000965fbd11a010b5753ccb8e84bc816fca
lalberto01 <lesleyalar@gmail.com>
2026-04-13 15:03:41 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,9 @@
 /*
 Sakila is a database used to collect infromation and used to practice. 
 */
+
+USE sakila;
+SELECT * FROM actor;-- Retrieved 200 records
+
+SELECT * FROM film;
+SELECT * FROM film;-- Retrieved 1000 records
