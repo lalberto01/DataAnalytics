58daf6f34dfb67f8e0e047299ab0ff9b526f1c3a
lalberto01 <lesleyalar@gmail.com>
2026-05-04 14:54:53 -0400

    Added answers

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,23 @@
 print ('Hello world!')
+
+message = 'Hello world!'
+print(message)
+
+# 'Hello world' prints twice because the variable holding is the same text.
+
+# Displaying dollars and cents
+
+dollars = 3
+cents = .50
+print(dollars+cents)
+
+# The result ended with a decimal because it started with a decimal.
+
+cents = cents+.25
+print(dollars+cents)
+
+# It added .25 to the answer before this.
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str+' '+c_str)
