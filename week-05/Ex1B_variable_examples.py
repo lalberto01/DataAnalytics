dc898e9c6111e620c9b0414a987f5f3b9e2b5cdb
lalberto01 <lesleyalar@gmail.com>
2026-05-04 15:42:42 -0400

    added comments

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,23 @@
+customer_id = 'A123456' # Id numbers usally contain a mix of number and letter. 
+customer_name = 'John Doe' # full name
+customer_gender = 'Female' # A text value 
+customer_date_of_birth = '01/01/2000' # used mm/dd/yyyy format 
+drivers_license_number = '009290' # usually starts with leading numbers
+auto_policy_number = 'L090909' # used numbers and letters
+
+name = 'Lesley Alberto'
+city = 'Concord'
+state = 'North Carolina'
+
+
+# The list of words that can't be used for variable names is False, def, if, raise
+# None, del, import, return, True, elif, in,try, and, else, is, while, as, except,
+# lambda, with, assert, finally, nonlocal, yield, break, for, not, class, form, or
+# continue, global, pass.
+
+# 1. false, represents opposite of true. 
+# 2. none, the absence of a value. Like an empty object.
+# 3. del, used to delete an object in python.
+# 4. return, exit a function or a method and return of some value. 
+# 5. is, used to check if the two variables are equal. 
+
