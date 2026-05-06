0cb050c12a4f7071fb9b1b2b302c9ce9eb49b62d
lalberto01 <lesleyalar@gmail.com>
2026-05-06 18:42:05 -0400

    Area of Rectangle

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,15 @@
+# Area of rectangle
+#Formula with birthdate 
+# Area = 7 x 21
+
+side_a = 7 #birthday month
+side_b = 21 # day of the month
+
+# Calculate the area
+area = side_a * side_b
+
+# Results
+print("Side A is " + str(side_a))
+print("Side B is " + str(side_b))
+print("The are of the rectangle is " + str(area))
+
