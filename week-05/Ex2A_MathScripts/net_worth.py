6f2c82eb134c1f5c48ca2882692ec9379349c69d
lalberto01 <lesleyalar@gmail.com>
2026-05-06 16:54:51 -0400

    Assets and debt

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,29 @@
+# Assets, things you own have value.
+# Debts money you owe. 
+
+# B) Net worth = Total Assets - Total Debts
+
+# Total Assets
+house = 180000.00
+car = 20000.00
+watch = 5000
+savings = 1000
+
+# Debts
+credit_card = 20000
+car_loan = 16000
+mortgage = 100000
+
+# Totals
+total_assets = house + car + watch + savings
+total_debts = credit_card + car_loan + mortgage 
+
+# Net worth
+net_worth = total_assets - total_debts
+
+# Results
+print("Your total assets are " + format(total_assets, ".2f"))
+print("Your total debts are " + format(total_debts, ".2f"))
+print("Your net worth is " + format(net_worth, ".2f"))
+
+
