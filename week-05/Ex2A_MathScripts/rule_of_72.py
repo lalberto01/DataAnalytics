13247918d78407d9c99d5f8a768395af9ec06fd4
lalberto01 <lesleyalar@gmail.com>
2026-05-07 17:12:42 -0400

    Lab 4 added

--- week-05/Ex2A_MathScripts/rule_of_72.py
+++ week-05/Ex2A_MathScripts/rule_of_72.py
@@ -18,3 +18,15 @@ print("Your current savings is", savings)
 print("At a", format(interest_rate, ".0%"),"interest rate, your savings account will be")
 print("worth", format(doubled_balance, ".2f"), "in", format(years, ".1f"), "years")
 
+# Lab 4
+# Tip Amount = Bill Total x (Tip Percentage / 100)
+
+# Ask user for values
+bill_total = float(input("How much was your restaurant bill? "))
+tip_percentage = float(input("What percentage do you want to tip? "))
+
+# Calculate tip
+tip_amount = bill_total * (tip_percentage / 100)
+
+# Output using f-string
+print(f"The tip on a ${bill_total:.2f} restaurant bill is ${tip_amount:.2f}")
