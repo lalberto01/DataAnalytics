ad48deaf5786f4c00bdc1f120270b7b132eb7c64
lalberto01 <lesleyalar@gmail.com>
2026-05-11 00:22:52 -0400

    added

---
+++ week-05/Ex4C_LoopScripts/loop_love.py
@@ -0,0 +1,9 @@
+# List of words that mean "great"
+great_words = ["amazing", "awesome", "excellent", "fantastic", "incredible"]
+
+# Loop through the list
+for word in great_words:
+    print(f"Loops are {word}!")
+
+# After the loop ends
+print("I <3 loops")
