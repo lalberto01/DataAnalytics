e83dc84b8db9f5a02602f8aa8fba47e6c4159275
lalberto01 <lesleyalar@gmail.com>
2026-05-10 23:21:56 -0400

    added

---
+++ week-05/Ex4A_IfScripts/pay_rules.py
@@ -0,0 +1,20 @@
+# Description: This script calculates gross pay with overtime
+# Author: Sam Q. Newprogrammer
+
+# Example values
+pay_rate = 17.30
+hours_worked = 45
+
+# Calculate gross pay
+if hours_worked <= 40:
+    gross_pay = pay_rate * hours_worked
+else:
+    regular_pay = pay_rate * 40
+    overtime_hours = hours_worked - 40
+    overtime_pay = overtime_hours * (pay_rate * 1.5)
+    gross_pay = regular_pay + overtime_pay
+
+# Results
+print(f"Pay rate: ${pay_rate:.2f}")
+print(f"Hours worked: {hours_worked}")
+print(f"Gross pay: ${gross_pay:.2f}")
