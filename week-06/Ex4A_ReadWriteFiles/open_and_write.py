a85229d67a4ff26019e5bbe56468501b1bf3f2bd
lalberto01 <lesleyalar@gmail.com>
2026-05-14 15:51:09 -0400

    "Saved 4A"

---
+++ week-06/Ex4A_ReadWriteFiles/open_and_write.py
@@ -0,0 +1,16 @@
+# Open txt file
+f = open("about_me.txt", "a")
+
+# close
+f.close()
+
+# Open
+f = open("about_me.txt", "a")
+
+# Add new information to the file
+f.write("\n\nIf you could do anything for your 'perfect' night out, where would you go and what would you do?\n")
+
+f.write("My perfect night out would be good food and nice warm weather.\n")
+
+# Close
+f.close()
