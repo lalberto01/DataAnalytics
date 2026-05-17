9e7b7e56793d1def88a074939c6444bb3b672280
lalberto01 <lesleyalar@gmail.com>
2026-05-17 11:58:36 -0400

    " updated 4A "

--- week-06/Ex4A_ReadWriteFiles/open_and_read.py
+++ week-06/Ex4A_ReadWriteFiles/open_and_read.py
@@ -3,3 +3,41 @@ f = open("about_me.txt", "r")
 print(f.read())
 
 f.close()
+
+f = open("about_me.txt", "r")
+
+print(f.read(50))
+
+print()
+
+print(f.read(50))
+
+f.close()
+
+# Readline 
+
+f = open("about_me.txt", "r")
+
+first_chars = f.read(50)
+
+next_lines = []
+
+for i in range(1, 5):
+    next_lines.append(f.readline())
+
+next_100 = f.readlines(100)
+
+print("First 50 characters:")
+print(first_chars)
+
+print()
+
+print("Next four lines, as list by line:")
+print(next_lines)
+
+print()
+
+print("Next 100 characters, as list by line, rounded up to complete lines:")
+print(next_100)
+
+f.close()
